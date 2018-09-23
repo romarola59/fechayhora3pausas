a46ba6a619dd18ed59ffd4b91ff1008ef6ee97de
Rosa M. Rodriguez <romarola@hotmail.com>
2018-09-22 23:55:12 -0300

    Agregar archivos de proyecto.

---
+++ Fechayhora3pausas.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace fechayhora3pausas
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            DateTime fecha = DateTime.Now;  // inicializamos la variable fecha, con la fecha actual.
+
+            Console.WriteLine("Son las {0}: {1}:{2} ",fecha.Hour,fecha.Minute,fecha.Second);
+            Console.WriteLine("Vamos a esperar 10 segundos .....");
+            Thread.Sleep(10000);  // comando para hacer la pausa en 5 segundos
+            fecha = DateTime.Now;  // actualizamos la fecha en Horas , minutos,segundos(en tiempo)
+            Console.WriteLine("Ahora son las {0}: {1}:{2} ", fecha.Hour, fecha.Minute, fecha.Second);
+            Console.WriteLine("Ahora vamos a esperar medio minuto....");
+            Thread.Sleep(30000);
+            fecha = DateTime.Now;
+            Console.WriteLine("Ahora despues de medio minuto son {0}: {1}:{2}", fecha.Hour, fecha.Minute, fecha.Second);
+            Console.WriteLine("hora de dormir byeeeeeeeee.....");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ fechayhora3pausas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace fechayhora3pausas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
